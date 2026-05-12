2902ec9be270d703de0b37ff81783f8c349fc9c4
redchriss <redsonngwira69@example.com>
2026-05-12 03:32:05 +0200

    fix(build): add NDK 27, core desugaring, missing asset dirs
    - Set ndkVersion to 27.0.12077973 (required by plugins)
    - Enable core library desugaring for flutter_local_notifications
    - Add multiDexEnabled to defaultConfig
    - Add desugar_jdk_libs dependency
    - Create missing assets/images/ and assets/lottie/ directories

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,12 @@ plugins {
 android {
     namespace = "mw.yaza.studyapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -20,14 +21,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "mw.yaza.studyapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -37,6 +36,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
