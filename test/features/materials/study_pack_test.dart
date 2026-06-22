1580d6542f573e73afd45dca97322df949982a49
redchriss <redsonngwira69@example.com>
2026-06-22 13:54:21 +0200

    fix(tests): add missing study_pack_data.dart import to study_pack_test

--- test/features/materials/study_pack_test.dart
+++ test/features/materials/study_pack_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:studyapp/features/materials/presentation/screens/study_pack_data.dart';
 import 'package:studyapp/features/materials/presentation/screens/study_pack_sheet.dart';
 
 StudyPackData _samplePack() {
